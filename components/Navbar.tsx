f72cad735d9d8e18b40b6777598f0066afbe104c
Thinh (Oscar) Le <oscarthinhle@gmail.com>
2025-09-16 18:07:36 +1000

    add Theme Menu for Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,6 @@
-import { LogOut, Moon, Settings, SquareMenu, User } from "lucide-react";
+"use client"
+
+import { LogOut, Moon, Settings, SquareMenu, Sun, User } from "lucide-react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import {
@@ -10,16 +12,38 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
+  const { theme, setTheme } = useTheme();
   return (<nav className="p-4 flex justify-between items-center">
     {/*LEFT SIDE*/}
     collapse button
     {/*RIGHT SIDE*/}
     <div className="flex items-center gap-4">
       <Link href="/">Dashboard</Link>
-      <Moon />
-
+      {/*THEME MENU*/}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {/*USER MENU*/}
       <DropdownMenu>
         <DropdownMenuTrigger><Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
